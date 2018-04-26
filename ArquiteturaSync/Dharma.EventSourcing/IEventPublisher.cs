a0697ddc053c7ae95df1f86dba92373826cfd3eb
Matheus Primo <matheussilvaprimo@hotmail.com>
2018-04-26 16:31:07 -0300

    IEventStore and IEventPublisher

---
+++ ArquiteturaSync/Dharma.EventSourcing/IEventPublisher.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Dharma.EventSourcing
+{
+    public interface IEventPublisher<TEvent> where TEvent : Event
+    {
+        Task PublishAsync(TEvent @event);
+    }
+}
---
+++ ArquiteturaSync/Dharma.EventSourcing/IEventStore.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Dharma.EventSourcing
+{
+    public interface IEventStore
+    {
+        Task SaveEventAsync(string aggregateId, Event @event);       
+        Task<List<Event>> GetEventsAsync(string aggregateId);
+    }
+}
