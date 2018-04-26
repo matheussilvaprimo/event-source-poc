85802a598f75c4327f62353ce3b9f5ed1371b9b6
Matheus Primo <matheussilvaprimo@hotmail.com>
2018-04-26 16:35:55 -0300

    Memmers.EventStore

---
+++ ArquiteturaSync/Members.EventStore/CommonMemberEvent.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Dharma.EventSourcing;
+
+namespace Members.EventStore
+{
+    public class CommonMemberEvent : Event
+    {
+        public CommonMemberEvent(string ID, DateTime Date, string FingerPrint, string Source) : base(ID, Date, FingerPrint, Source) { }
+    }
+}
---
+++ ArquiteturaSync/Members.EventStore/MemberAggregateRoot.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Dharma.EventSourcing;
+
+namespace Members.EventStore
+{
+    public class MemberAggregateRoot : AggregateRoot<Event>
+    {
+        string MemberID { get; set; }
+        string LegacyID { get; set; }
+        public override void AddEventToStream(Event e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RebuildEventStream()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
