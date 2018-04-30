a788e339da69fc227fa721a63c3d7d7bb5cdb5c3
rubensvadesouza <rubensvadesouza@hotmail.com>
2018-04-30 15:45:51 -0300

    Criação dos testes unitários MemverCreateHandlerTest

---
+++ ArquiteturaSync/Members.Sync.Next.EventSourcing.Tests/MemberCreatedHandlerTest.cs
@@ -0,0 +1,44 @@
+﻿using Members.Sync.Next.EventSourcing.Domain.Events;
+using Members.Sync.Next.EventSourcing.Domain.Handlers;
+using Members.Sync.Next.EventSourcing.Infra;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using Xunit;
+
+namespace Members.Sync.Next.EventSourcing.Tests
+{
+    public class MemberCreatedHandlerTest
+    {
+        public MemberCreatedHandlerTest()
+        {
+        }
+
+        private ServiceProvider Setup()
+        {
+            var serviceProvider = new ServiceCollection()
+                                     .AddScoped<MemberCreatedHandler>()
+                                     .AddScoped<ICassandraEventStore, CassandraEventStore>()
+                                     .AddScoped<CassandraProvider>();
+
+            return serviceProvider.BuildServiceProvider();
+        }
+
+        [Fact]
+        public void HandleMemberWithMessageNull()
+        {
+            var provider = Setup();
+            var handler = provider.GetService<MemberCreatedHandler>();
+            Assert.ThrowsAsync<NotImplementedException>(() => handler.HandleMemberAsync(null));
+        }
+
+        [Fact]
+        public void HandleMemberWithMessage()
+        {
+            var provider = Setup();
+            var handler = provider.GetService<MemberCreatedHandler>();
+            var message = new MemberCreatedEvent("", "", DateTime.Now, "");
+
+            Assert.ThrowsAsync<NotImplementedException>(() => handler.HandleMemberAsync(message));
+        }
+    }
+}
