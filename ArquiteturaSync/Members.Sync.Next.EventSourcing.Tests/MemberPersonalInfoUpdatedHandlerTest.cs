6e27cc45d81bb4084df6edb513fc655e51433774
Matheus Primo <matheussilvaprimo@hotmail.com>
2018-05-02 10:36:43 -0300

    removing namespaces

--- ArquiteturaSync/Members.Sync.Next.EventSourcing.Tests/MemberPersonalInfoUpdatedHandlerTest.cs
+++ ArquiteturaSync/Members.Sync.Next.EventSourcing.Tests/MemberPersonalInfoUpdatedHandlerTest.cs
@@ -1,8 +1,6 @@
-﻿using Members.Sync.Next.EventSourcing.Domain.Events;
-using Members.Sync.Next.EventSourcing.Domain.Handlers;
+﻿using Members.Sync.Next.EventSourcing.Domain.Handlers;
 using Members.Sync.Next.EventSourcing.Infra;
 using Microsoft.Extensions.DependencyInjection;
-using System;
 using Xunit;
 
 namespace Members.Sync.Next.EventSourcing.Tests
