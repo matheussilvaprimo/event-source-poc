5001d4a6a100f2f8af24c15a58256732d5ef1429
Matheus Primo <matheussilvaprimo@hotmail.com>
2018-09-01 12:10:08 -0300

    eventstore filter extension

--- infra/eventstore/Extensions/FilterExtensions.cs
+++ infra/eventstore/Extensions/FilterExtensions.cs
@@ -15,12 +15,57 @@ namespace EventStore.Extensions
 
             var filter = new Filter();
 
-            foreach(var field in properties)
-            {                
-                filter = Filter.Equal(field.Name, (string)field.GetValue(o));
+            foreach (var field in properties)
+            {
+                filter = Filter.Equal(field.Name, GetValueFromType(field.GetValue(o)));
             }
 
             return new Filter(filter);
+        }
+
+        private static Value GetValueFromType(object o)
+        {
+            var type = o.GetType();
+            Value val = null;
+
+            if (type == typeof(string))
+            {
+                val = (string)o;
+            }
+            else if (type == typeof(Guid))
+            {
+                val = ((Guid)o).ToString();
+            }
+            else if (type == typeof(DateTime))
+            {
+                val = ((DateTime)o).ToUniversalTime();
+            }
+            else if (type == typeof(int))
+            {
+                val = (int)o;
+            }
+            else if (type == typeof(long))
+            {
+                val = (long)o;
+            }
+            else if (type == typeof(byte))
+            {
+                val = (byte)o;
+            }
+            else if (type == typeof(Enum))
+            {
+                val = (byte)o;
+            }
+            else if (type == typeof(bool))
+            {
+                val = (bool)o;
+            }
+            else if (type == typeof(double))
+            {
+                val = (double)o;
+            }
+
+            return val;
         }
     }
 }
